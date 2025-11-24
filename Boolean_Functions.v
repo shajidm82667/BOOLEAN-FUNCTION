d4a6dcc526a452efb4da746c0e719e2d3197b94b
shajidm82667 <ytgvx7ybkx@privaterelay.appleid.com>
2025-11-24 21:29:26 +0530

    Add files via upload

---
+++ Boolean_Functions.v
@@ -0,0 +1,11 @@
+module Boolean_Functions (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    input  wire D,
+    output wire F
+);
+
+assign F = (~A & B) | (C & D) | (A & ~D);
+
+endmodule
